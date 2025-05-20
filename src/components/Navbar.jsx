26fdac7b42138895c1d3a512e708db807f3ec9e5
CaarlosDev <crt.981995@gmail.com>
2025-05-20 18:24:26 +0000

    empezando proyecto

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
 
 	return (
-		<nav className="navbar navbar-light bg-light">
+		<nav className="navbar navbar-dark bg-dark">
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQ1BjFcON4mgqG-wdIJ3lUpuFFLsgTI3j7hg&s" alt="not found" style= {{height: '3rem', borderRadius: '20px' }}/>
 				</Link>
 				<div className="ml-auto">
 					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
+						<button className="btn btn-danger">Favoritos <i class="fa-brands fa-jedi-order"></i></button>
 					</Link>
 				</div>
 			</div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,24 @@
-import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card, Button } from 'react-bootstrap';
 
-export const Home = () => {
-
-  const {store, dispatch} =useGlobalReducer()
-
+function CardsViwer() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
+		<div className="container m-5 p-5">
+			<Card style={{ width: '18rem' }}>
+				<Card.Img variant="top" src="holder.js/100px180" />
+				<Card.Body>
+					<Card.Title>Card Title</Card.Title>
+					<Card.Text>
+						Some quick example text to build on the card title and make up the
+						bulk of the card's content.
+					</Card.Text>
+					<Button variant="primary">Go somewhere</Button>
+				</Card.Body>
+			</Card>
 		</div>
 	);
-}; 
+}
+
+export default CardsViwer;
+
