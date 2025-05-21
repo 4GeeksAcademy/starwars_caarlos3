a22815f13077db9f5dcc67a16ac9f6ebd473d7a9
CaarlosDev <crt.981995@gmail.com>
2025-05-21 19:28:12 +0000

    agregando personajes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,56 @@
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect } from "react";
 import { Card, Button } from 'react-bootstrap';
 
+
 function CardsViwer() {
+
+	const { store, dispatch } = useGlobalReducer();
+
+	useEffect(() => {
+		const fetchPeople = async () => {
+			const peopleResponse = await fetch("https://www.swapi.tech/api/people/")
+			const data = await peopleResponse.json();
+			const completeCharacter = await Promise.all(
+				data.results.map(async(person) => {
+					const res = await fetch(`https://www.swapi.tech/api/people/${person.uid}/`);
+					const detail = await res.json();
+					return{
+						uid: person.uid,
+						name: person.name,
+						...detail.result.properties
+					};
+			
+				})
+			)
+			dispatch({ type: 'CHARACTERS_ADD', payload: completeCharacter })
+		}
+		fetchPeople();
+	}, [])
+
 	return (
-		<div className="container m-5 p-5">
-			<Card style={{ width: '18rem' }}>
-				<Card.Img variant="top" src="holder.js/100px180" />
-				<Card.Body>
-					<Card.Title>Card Title</Card.Title>
-					<Card.Text>
-						Some quick example text to build on the card title and make up the
-						bulk of the card's content.
-					</Card.Text>
-					<Button variant="primary">Go somewhere</Button>
-				</Card.Body>
-			</Card>
-		</div>
+
+		<section>
+			<h3 className="m-5">Personajes</h3>
+			<div className="container m-5 p-5">
+				<div className="d-flex flex-row gap-3 overflow-auto">
+					{store.characters?.map((personajes, index) => (
+						<Card key={index} style={{ width: '90rem' }}>
+							<Card.Img variant="top" src="holder.js/100px180" />
+							<Card.Body>
+								<Card.Title>{personajes.name}</Card.Title>
+								<Card.Text>
+									
+								</Card.Text>
+								<Button variant="primary">Go somewhere</Button>
+							</Card.Body>
+						</Card>
+					))}
+				</div>
+			</div>
+		</section>
+
 	);
 }
 
--- src/store.js
+++ src/store.js
@@ -1,32 +1,21 @@
-export const initialStore=()=>{
-  return{
+export const initialStore = () => {
+  return {
     message: null,
-    todos: [
-      {
-        id: 1,
-        title: "Make the bed",
-        background: null,
-      },
-      {
-        id: 2,
-        title: "Do my homework",
-        background: null,
-      }
-    ]
-  }
+    characters: [],
+    planets: [],
+    vehicles: []
+  };
 }
 
 export default function storeReducer(store, action = {}) {
-  switch(action.type){
-    case 'add_task':
-
-      const { id,  color } = action.payload
-
-      return {
-        ...store,
-        todos: store.todos.map((todo) => (todo.id === id ? { ...todo, background: color } : todo))
-      };
+  switch (action.type) {
+    case 'CHARACTERS_ADD':
+      return { ...store, characters: action.payload };
+    case 'PLANETS_ADD':
+      return { ...store, planets: action.payload };
+    case 'VEHICLES_ADD':
+      return { ...store, vehicles: action.payload };
     default:
       throw Error('Unknown action.');
-  }    
+  }
 }
